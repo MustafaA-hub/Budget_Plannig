26913c9f5b273e362b8e2cbba83753b5ab2c7ebb
MustafaA-hub <70828506+MustafaA-hub@users.noreply.github.com>
2025-01-26 20:56:41 -0500

    Update Main_window.py

--- Main_window.py
+++ Main_window.py
@@ -11,6 +11,7 @@ from PyQt5.QtWidgets import (
 )
 import openpyxl
 from Categories import Categories
+from Data_Plots import plot_spending
 
 class MA_Finance(QMainWindow):
     def __init__(self):
@@ -30,6 +31,10 @@ class MA_Finance(QMainWindow):
         self.load_button.clicked.connect(self.load_excel_file)
         self.layout.addWidget(self.load_button)
 
+        self.plot_button = QPushButton('Open Plot Window', self)
+        self.plot_button.clicked.connect(self.open_plot_window)
+        self.layout.addWidget(self.plot_button)
+
         # Add a table to display Excel content
         self.table = QTableWidget()
         self.layout.addWidget(self.table)
@@ -41,11 +46,11 @@ class MA_Finance(QMainWindow):
             self, "Import Statement", "", "Excel Files (*.xlsx *.xls *.csv)"
         )
         if file_path:
-            #self.display_excel_content(file_path)
+            #self.load_format_content(file_path)
         """
-        self.display_excel_content(r"C:\Users\jasle\OneDrive\Documents\GitHub\Budget_Plannig\2024_Chequing_Transactions_only.xlsx")
+        self.load_format_content(r"C:\Users\jasle\OneDrive\Documents\GitHub\Budget_Plannig\2024_Chequing_Transactions_only.xlsx")
 
-    def display_excel_content(self, file_path):
+    def load_format_content(self, file_path):
         # Load the Excel file
         try:
             workbook = openpyxl.load_workbook(file_path)
@@ -76,32 +81,40 @@ class MA_Finance(QMainWindow):
             # Add a column to the table named category and move it to the first column
             self.table.insertColumn(0)
             self.table.setHorizontalHeaderItem(0, QTableWidgetItem("Category"))
-            
+
+            # Remove columns that are not needed
+            columns_to_keep = ["category", "date", "sub-description", "amount", "balance"]
             columns_to_remove = []
             for col in range(self.table.columnCount()):
                 header_item = self.table.horizontalHeaderItem(col)
-                if header_item and header_item.text().lower() in ["status", "filter"]:
+                #if header_item and header_item.text().lower() in ["status", "filter", "description", "type of transaction", "None"]:
+                if header_item and header_item.text().lower() not in columns_to_keep:
                     columns_to_remove.append(col)
 
             # Remove the columns in reverse order to avoid shifting issues
             for col in sorted(columns_to_remove, reverse=True):
                 self.table.removeColumn(col)
             
-            # Check if any cells in column 4 are empty and set them to "Not Specified"
+            # Check if any cells in sub-description column are empty and set them to "Not Specified"
             for row in range(self.table.rowCount()):
-                item = self.table.item(row, 3)
+                item = self.table.item(row, 2)
                 if item is None or item == "None" or not item.text().strip():
-                    self.table.setItem(row, 3, QTableWidgetItem("Not Specified"))
+                    self.table.setItem(row, 2, QTableWidgetItem("Not Specified"))
                     self.table.setItem(row, 0, QTableWidgetItem("Not Specified"))
 
-            table = self.table
             Categories(self.table)
             
+            # Remove columns that are not needed
+            self.table.removeColumn(5)
+            self.table.removeColumn(5)
+
         except Exception as e:
             print(f"Error loading Excel file: {e}")
-    
 
-
+    def open_plot_window(self):
+        self.plot_window = plot_spending(self.table, self)
+        self.plot_window.show()
+        
 # Run the application
 if __name__ == "__main__":
     app = QApplication(sys.argv)
